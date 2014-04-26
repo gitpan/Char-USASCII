db42822865315673c159b4b59b03fd4cdb9466cf
INABA Hitoshi <ina@cpan.org>
2014-04-26 05:21:39 +0000

    Import of INA/Char-USASCII-0.96 from CPAN.
    gitpan-cpan-distribution: Char-USASCII
    gitpan-cpan-version:      0.96
    gitpan-cpan-path:         INA/Char/USASCII/Char-USASCII-0.96.tar.gz
    gitpan-cpan-author:       INA
    gitpan-cpan-maturity:     released

--- Char/Eusascii.pm
+++ Char/Eusascii.pm
@@ -17,7 +17,7 @@ use 5.00503;    # Galapagos Consensus 1998 for primetools
 # (and so on)
 
 BEGIN { eval q{ use vars qw($VERSION) } }
-$VERSION = sprintf '%d.%02d', q$Revision: 0.95 $ =~ /(\d+)/xmsg;
+$VERSION = sprintf '%d.%02d', q$Revision: 0.96 $ =~ /(\d+)/xmsg;
 
 BEGIN {
     if ($^X =~ / jperl /oxmsi) {
@@ -1844,6 +1844,9 @@ sub charlist_qr {
                         }
                     }
                 }
+                if ($_ ne '') {
+                    $singleoctet_ignorecase{unpack 'C*', $_} = 1;
+                }
             }
             my $i = 0;
             my @singleoctet_ignorecase = ();
@@ -1918,6 +1921,9 @@ sub charlist_not_qr {
                         }
                     }
                 }
+            }
+            if ($_ ne '') {
+                $singleoctet_ignorecase{unpack 'C*', $_} = 1;
             }
         }
         my $i = 0;
@@ -2322,6 +2328,12 @@ OUTER:
         my $pattern = '';
         while ($expr =~ / \G ($q_char) /oxgc) {
             my $char = $1;
+
+            # 6.9. Matching Shell Globs as Regular Expressions
+            # in Chapter 6. Pattern Matching
+            # of ISBN 0-596-00313-7 Perl Cookbook, 2nd Edition.
+            # (and so on)
+
             if ($char eq '*') {
                 $pattern .= "(?:$your_char)*",
             }
@@ -3226,13 +3238,13 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
   @ignorecase = Char::Eusascii::ignorecase(@string);
 
-  This subroutine is internal use to m/ /i, s/ / /i, split / /i and qr/ /i.
+  This subroutine is internal use to m/ /i, s/ / /i, split / /i, and qr/ /i.
 
 =item Make capture number
 
   $capturenumber = Char::Eusascii::capture($string);
 
-  This subroutine is internal use to m/ /, s/ / /, split / / and qr/ /.
+  This subroutine is internal use to m/ /, s/ / /, split / /, and qr/ /.
 
 =item Make character
 
